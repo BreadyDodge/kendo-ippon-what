915a9607f36faee999e82b12c327627ce4d562ae
Tengku Fabian Hadra <87185858+BreadyDodge@users.noreply.github.com>
2025-01-21 13:36:30 +0700

    adding new files

---
+++ inquiry.py
@@ -0,0 +1,22 @@
+#ask an input of ippon
+#count the person with the most point scored in that waza 
+from collections import defaultdict
+import csv
+
+kendo_ippon = []
+with open("70th mens AJKC.csv",'r',encoding='utf-8-sig') as file:
+    excelfile = csv.reader(file)
+    kendo_ippon = list(excelfile)
+
+search = input("What do you want to see? ")
+names = defaultdict(int)
+categories = kendo_ippon.pop(0)
+for match in kendo_ippon:
+  if match[categories.index("Target")] == search:
+    if match[categories.index("Point_awarded_to")] == "White":
+        names[match[categories.index("Competitor_White")]] += 1
+    elif match[categories.index("Point_awarded_to")] == "Red":
+        names[match[categories.index("Competitor_Red")]] += 1
+
+winner = max(names, key=names.get)
+print(f'The fighter with the most amount of {search} is {winner} with {names[winner]} points scored')
