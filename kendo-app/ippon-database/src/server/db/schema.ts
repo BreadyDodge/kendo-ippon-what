86b70669d895c59bddbcfb51d93eab0d32d7b219
Tengku Fabian Hadra <87185858+BreadyDodge@users.noreply.github.com>
2025-05-05 23:30:39 +0700

    chore: setting up database

--- kendo-app/ippon-database/src/server/db/schema.ts
+++ kendo-app/ippon-database/src/server/db/schema.ts
@@ -2,7 +2,8 @@
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
 import { sql } from "drizzle-orm";
-import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
+import { boolean } from "drizzle-orm/mysql-core";
+import { int, sqliteTableCreator, text, check } from "drizzle-orm/sqlite-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -10,21 +11,35 @@ import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = sqliteTableCreator((name) => `ippon-database_${name}`);
-
-export const posts = createTable(
-  "post",
-  {
-    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-    name: text("name", { length: 256 }),
-    createdAt: int("created_at", { mode: "timestamp" })
-      .default(sql`(unixepoch())`)
-      .notNull(),
-    updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+export const createTable = sqliteTableCreator(
+  (name) => `ippon-database_${name}`,
 );
+
+export const tournamentTable = createTable("tournaments", {
+  id: text("id").primaryKey().notNull(),
+  name: text("name").notNull(),
+  year: int("year").notNull(),
+  rounds: int("rounds").notNull(),
+});
+
+export const matchesTable = createTable("matches", {
+  id: text("id").primaryKey(),
+  tournament: text("tournament").notNull(),
+  year: int("year").notNull(),
+  round: int("round").notNull(),
+  redTeam: text("red_team"),
+  redPlayer: text("red_player").notNull(),
+  whiteTeam: text("white_team"),
+  whitePlayer: text("white_player").notNull(),
+  pointAwardedTo: text("point_awarded_to").notNull(),
+  order: int("order").notNull(),
+  target: text("target").notNull(),
+  waza: text("waza"),
+  encho: text("encho").default("N"),
+  createdAt: int("created_at", { mode: "timestamp" })
+    .default(sql`(unixepoch())`)
+    .notNull(),
+  updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
+    () => new Date(),
+  ),
+});
